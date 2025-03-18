a67c79d51848427a75808752297997d1a2c10bc9
ItzBlack <100058443+ItzBlack6093@users.noreply.github.com>
2025-03-18 22:14:36 +0700

    added reset promotion delay

--- src/mechanics/gamemode_extended.js
+++ src/mechanics/gamemode_extended.js
@@ -88,6 +88,10 @@ export class Zenith {
                         }
                     }
 
+                if(this.isLastRankChangePromote && this.climbPoints >= 2*(t-1)){
+            this.promotionFatigue = 0
+                      }
+
                 //calculate stats
                     Game.stats.climbSpeed = t + this.climbPoints / (4 * t);
                     this.altitude += o / 60 * a
